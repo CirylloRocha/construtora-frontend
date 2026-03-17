74fd3e87d645697fa51d3512a0a1e4e24466b63b
CirylloRocha <ciryllorocha@gmail.com>
2026-03-16 23:38:52 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // IMPORTANTE: Substitua pelos seus dados reais do Supabase
 
 const SUPABASE_URL = 'https://xfageuqkfzatdbpxugcw.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_secret_ZFxPqz0fTGlVsCTeqV2K5A_VIM8cR3r';
+const SUPABASE_ANON_KEY = 'sb_publishable_FAnbLZkMdgVzOWAhTi3Iag_vNhcxZ3t';
 
 // Importar cliente Supabase
 const { createClient } = supabase;
